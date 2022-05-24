0c88c4432b7bd600bc5e1dbe73d51ab50b5b68ea
John Quinn <jmquinn2312@gmail.com>
2022-05-24 09:17:54 -0700

    fixed user undefined

--- src/App.js
+++ src/App.js
@@ -149,14 +149,23 @@ loadUser = (data) => {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})  
+onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
     app.models
       .predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
+    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+    // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+    // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+    // so you would change from:
+    // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    // to:
+    // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
       .then(response => {
-        if(response) {
+        if (response) {
           fetch('http://localhost:3001/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -164,11 +173,17 @@ loadUser = (data) => {
               id: this.state.user.id
             })
           })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
         }
+        this.calculateFaceLocation(response)
       })
-    .then(response => this.calculateFaceLocation(response))
-  
+      .catch(err => console.log(err));
   }
+
 
   onRouteChange = (route) => {
     if (route === 'signout') {
